6bff5e1f2575b5df9577aa4dd18c4d8d4588caf1
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-13 03:00:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(num)
   int = num.abs
-  return false if (int == 0) || (int == 1)
+  return false if (int == 0) 
   return true if int == 2
   search_array = (2...int).to_a
   if search_array.any?{|x| int%x == 0}
