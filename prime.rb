27a57db5e0d007ef4f556ff257f96e7d5d353b11
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-13 03:01:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  int = num.abs
+  return false if (int == 0) || (int == 1)
+  return true if int == 2
+  search_array = (2...int).to_a
+  if search_array.any?{|x| int%x == 0}
+    return false 
+  else
+    return true
+  end
+end
